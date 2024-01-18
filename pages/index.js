52de27a55b9a83408089d9d68373b5c391b150c5
YavorLalev <yavorlalev@abv.bg>
2024-01-18 21:58:38 +0100

    Add Head Component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Link from "next/link";
 import { introduction } from "resources/lib/data";
+import Head from "next/head";
 
 export default function HomePage() {
   return (
-    <div>
+    <>
+      <Head>
+        <title>Lord of the rings App</title>
+      </Head>
+
       <h1>Lord of the Rings</h1>
       <p>{introduction}</p>
       <Link href="/volumes">Lord of the Rings: Volume Overview</Link>
-    </div>
+    </>
   );
 }
